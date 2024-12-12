c9988bb798dfa8b41e601371f028c4c2ca221fe8
ffffffoi <takoyaki.work1234@gmail.com>
2024-12-12 14:27:35 +0900

    tera

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use axum::{
   extract::ConnectInfo,
   routing,
+  response::Html,
   Router
 };
 use std::net::SocketAddr;
+use tera::{Context,Tera};
 
-async fn handler(ConnectInfo(ip): ConnectInfo<SocketAddr>) -> String {
-  format!("IP: {ip}")
+async fn handler(ConnectInfo(ip): ConnectInfo<SocketAddr>) -> Html<String> {
+  let tera = Tera::new("assets/*.html").unwrap();
+  let mut ctx = Context::new();
+  ctx.insert("IP", &ip.to_string());
+
+  Html(tera.render("index.html", &ctx).unwrap().to_owned())
 }
 
 #[tokio::main]
